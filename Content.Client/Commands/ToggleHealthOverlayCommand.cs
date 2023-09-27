9108f790f04c4627c5e46ca5e59303ead79cafeb
Repo <47093363+Titian3@users.noreply.github.com>
2023-09-27 18:57:24 +1300

    Send a message to client to turn it off.

--- Content.Client/Commands/ToggleHealthOverlayCommand.cs
+++ Content.Client/Commands/ToggleHealthOverlayCommand.cs
@@ -7,14 +7,35 @@ namespace Content.Client.Commands
     public sealed class ToggleHealthOverlayCommand : IConsoleCommand
     {
         public string Command => "togglehealthoverlay";
-        public string Description => "Toggles a health bar above mobs.";
-        public string Help => $"Usage: {Command}";
+        public string Description => "Toggles a health bar above mobs. If given bool as argument will set that state.";
+        public string Help => $"Usage: {Command} [bool]";
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
-            var system = EntitySystem.Get<HealthOverlaySystem>();
-            system.Enabled = !system.Enabled;
+            if (args.Length > 1)
+            {
+                shell.WriteLine(Help);
+                return;
+            }
 
+            var system = EntitySystem.Get<HealthOverlaySystem>();
+
+            if (args.Length == 0)
+            {
+                system.Enabled = !system.Enabled;
+            }
+            else
+            {
+                if (bool.TryParse(args[0], out var isEnabled))
+                {
+                    system.Enabled = isEnabled;
+                }
+                else
+                {
+                    shell.WriteLine("Invalid value. Expected true or false.");
+                    return;
+                }
+            }
             shell.WriteLine($"Health overlay system {(system.Enabled ? "enabled" : "disabled")}.");
         }
     }
--- Content.Server/Administration/Commands/DeAdminCommand.cs
+++ Content.Server/Administration/Commands/DeAdminCommand.cs
@@ -1,8 +1,12 @@
+using System.Threading.Tasks;
 using Content.Server.Administration.Managers;
+using Content.Server.Database;
+using Content.Server.Players;
 using Content.Shared.Administration;
 using JetBrains.Annotations;
 using Robust.Server.Player;
 using Robust.Shared.Console;
+using Robust.Shared.Network.Messages;
 
 
 namespace Content.Server.Administration.Commands
@@ -15,7 +19,7 @@ namespace Content.Server.Administration.Commands
         public string Description => "Temporarily de-admins you so you can experience the round as a normal player.";
         public string Help => "Usage: deadmin\nUse readmin to re-admin after using this.";
 
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        public async void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var player = shell.Player as IPlayerSession;
             if (player == null)
@@ -24,8 +28,20 @@ namespace Content.Server.Administration.Commands
                 return;
             }
 
+            await ExecuteClientSideCommand(shell); // Await the command to complete
+
             var mgr = IoCManager.Resolve<IAdminManager>();
             mgr.DeAdmin(player);
+        }
+
+        public async Task<bool> ExecuteClientSideCommand(IConsoleShell shell)
+        {
+            var testcommand = new MsgConCmd();
+            testcommand.Text = "togglehealthoverlay false";
+            if (shell.Player != null)
+                shell.Player.ConnectedClient.SendMessage(testcommand);
+            shell.Player.ConnectedClient.
+            return true;
         }
     }
 }
