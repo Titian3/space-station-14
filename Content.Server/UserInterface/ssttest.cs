33a49bb9e65157684367e560585568cf5bfc9236
Repo <47093363+Titian3@users.noreply.github.com>
2023-12-03 02:01:43 +1300

    the rest of it

--- Content.Server/UserInterface/ssttest.cs
+++ Content.Server/UserInterface/ssttest.cs
@@ -1,6 +1,40 @@
+using System.Speech.Recognition;
 namespace Content.Server.UserInterface;
 
-public sealed class ssttest
+
+public sealed class SpeechRecognitionWrapper
 {
-    
+    [Dependency] private SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+
+    public event Action<string>? SpeechRecognized;
+
+    public SpeechRecognitionWrapper()
+    {
+        InitializeRecognizer();
+    }
+
+    private void InitializeRecognizer()
+    {
+        _recognizer = new SpeechRecognitionEngine();
+        // Configure the recognizer (load grammar, etc.)
+        _recognizer.SpeechRecognized += OnRecognizerSpeechRecognized;
+        // Other setup...
+    }
+
+    private void OnRecognizerSpeechRecognized(object? sender, SpeechRecognizedEventArgs e)
+    {
+        SpeechRecognized?.Invoke(e.Result.Text);
+    }
+
+    public void StartRecognition()
+    {
+        _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+    }
+
+    public void StopRecognition()
+    {
+        _recognizer.RecognizeAsyncStop();
+    }
+
+    // Dispose pattern if necessary
 }
--- Content.Shared/UserInterface/SttTest.cs
+++ Content.Shared/UserInterface/SttTest.cs
@@ -1,6 +1,48 @@
-namespace Content.Shared.UserInterface;
+using System.Globalization;
+using System.Speech.Recognition;
 
-public sealed class SttTest
+namespace Content.Shared.UserInterface
 {
-    
+    public class SpeechRecognitionWrapper
+    {
+        [Dependency] private SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+
+        public event Action<string>? SpeechRecognized;
+
+        public SpeechRecognitionWrapper()
+        {
+            InitializeRecognizer();
+        }
+
+        private void InitializeRecognizer()
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            _recognizer = new SpeechRecognitionEngine(culture);
+
+            _recognizer.SetInputToDefaultAudioDevice();
+
+            Grammar dictationGrammar = new DictationGrammar();
+            _recognizer.LoadGrammar(dictationGrammar);
+
+            // Configure the recognizer (event handlers, etc.)
+            _recognizer.SpeechRecognized += OnRecognizerSpeechRecognized;
+        }
+
+        private void OnRecognizerSpeechRecognized(object? sender, SpeechRecognizedEventArgs e)
+        {
+            SpeechRecognized?.Invoke(e.Result.Text);
+        }
+
+        public void StartRecognition()
+        {
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        public void StopRecognition()
+        {
+            _recognizer.RecognizeAsyncStop();
+        }
+
+        // Dispose pattern if necessary
+    }
 }
